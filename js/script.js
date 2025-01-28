b948c5c530dc7165290d0eaa1d22ddbd5cc05f05
Keisy <k22.lizandra@gmail.com>
2025-01-28 20:42:04 -0300

    feat: Add lazy loading for Pokemon image and fetch data from PokeAPI

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const pokemonName = document.querySelector('.pokemon-name')
+const pokemonNumber = document.querySelector('.pokemon-number')
+const pokemonImage = document.querySelector('.pokemon-image')
+
+const form = document.querySelector('.form')
+
+const fetchPokemon = async (pokemon) => {
+
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data
+}
+
+const randerPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon)
+
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+}
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefalut();
+
+    co
+})
+
+randerPokemon('rayquaza')
