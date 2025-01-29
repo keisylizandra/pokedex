566da0ff90a89ebd680f6bc616fc7eede1ee54c3
Keisy <k22.lizandra@gmail.com>
2025-01-28 22:10:03 -0300

    chore: Update Pokedex page layout and functionality

--- js/script.js
+++ js/script.js
@@ -3,27 +3,63 @@ const pokemonNumber = document.querySelector('.pokemon-number')
 const pokemonImage = document.querySelector('.pokemon-image')
 
 const form = document.querySelector('.form')
+const input = document.querySelector('.input-search')
+const buttonPrev = document.querySelector('.btn-prev') 
+const buttonNext = document.querySelector('.btn-next') 
+
+let searchPokemon = 1
 
 const fetchPokemon = async (pokemon) => {
 
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-    return data
+
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data
+    }
+    
 }
 
-const randerPokemon = async (pokemon) => {
+const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
     const data = await fetchPokemon(pokemon)
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    if (data) {
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = ''
+        searchPokemon = data.id;
+    } else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = 'Not found :('
+        pokemonNumber = '';
+    }
+    
 }
 
 form.addEventListener('submit', (event) => {
 
-    event.preventDefalut();
+    event.preventDefault();
 
-    co
+    renderPokemon(input.value.toLowerCase());
 })
 
-randerPokemon('rayquaza')
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon -= 1
+        renderPokemon(searchPokemon)
+    }
+})
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1
+    renderPokemon(searchPokemon)
+})
+
+renderPokemon(searchPokemon);
+
